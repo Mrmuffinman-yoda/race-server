6a47b2d780abafed5d7c8255f865482d745cf44b
Harison Sundaramoorthy <harisoncom@gmail.com>
2024-12-23 19:37:01 +0000

    Remove build, crow, and .vscode directories from Git tracking

---
+++ build/CMakeFiles/RaceServer.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Projects/F1 Management/Code/Server/src/controllers/f1_controller.cpp" "CMakeFiles/RaceServer.dir/src/controllers/f1_controller.cpp.obj" "gcc" "CMakeFiles/RaceServer.dir/src/controllers/f1_controller.cpp.obj.d"
+  "E:/Projects/F1 Management/Code/Server/src/main.cpp" "CMakeFiles/RaceServer.dir/src/main.cpp.obj" "gcc" "CMakeFiles/RaceServer.dir/src/main.cpp.obj.d"
+  "E:/Projects/F1 Management/Code/Server/src/models/Driver.cpp" "CMakeFiles/RaceServer.dir/src/models/Driver.cpp.obj" "gcc" "CMakeFiles/RaceServer.dir/src/models/Driver.cpp.obj.d"
+  "E:/Projects/F1 Management/Code/Server/src/models/Team.cpp" "CMakeFiles/RaceServer.dir/src/models/Team.cpp.obj" "gcc" "CMakeFiles/RaceServer.dir/src/models/Team.cpp.obj.d"
+  "E:/Projects/F1 Management/Code/Server/src/routes/f1_routes.cpp" "CMakeFiles/RaceServer.dir/src/routes/f1_routes.cpp.obj" "gcc" "CMakeFiles/RaceServer.dir/src/routes/f1_routes.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
